9dfa7535017d1dc015d5ca5a06c45f2d04d91f01
Osman yousif <gamemakerx5@gmail.com>
2024-07-27 18:35:42 +0300

    added bot token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Telegram = require("node-telegram-bot-api");
-var token = "";
+var token = "6427776134:AAEXVE835pPoGyXPQaZZCS7mDtvpyKLh_5U";
 var opt = {polling: true};
 
 var bot = new Telegram(token, opt);
